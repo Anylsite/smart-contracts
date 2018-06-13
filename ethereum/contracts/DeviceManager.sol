79819c8cdca6e5fb7fe61a11fd239c9a9995cf2b
Bogdan Djukic <bdjukic@gmail.com>
2018-06-13 17:15:36 +0200

    Extending the functionality of the Device Manager smart contract. We are now tracking the mapping between device IDs and the most recent IPFS hash which represent the state of the device. Also added APIs for querying the list of registered devices which will be used by the AnyLedger Hub web app.

--- ethereum/contracts/DeviceManager.sol
+++ ethereum/contracts/DeviceManager.sol
@@ -1,9 +1,29 @@
 pragma solidity ^0.4.8;
 
 contract DeviceManager {
-    string public lastRegistration;
+    mapping(string => string) private registeredDevices;
+    string[] private registeredDeviceIds;
     
-    function setLastRegistration(string tempLastRegistration) public {
-        lastRegistration = tempLastRegistration;
+    function updateDeviceRegistration(string deviceId, string ipfsHash) public {
+        // Check if the device has been already registered.
+        if (isStringEmpty(registeredDevices[deviceId])) {
+            registeredDeviceIds.push(deviceId);
+        }
+        
+        registeredDevices[deviceId] = ipfsHash;
+    }
+    
+    function getDeviceRegistrationCount() public constant returns (uint) {
+        return registeredDeviceIds.length;
+    }
+    
+    function getDeviceRegistrationByIndex(uint deviceRegistrationIndex) public returns (string, string) {
+        string storage deviceId = registeredDeviceIds[deviceRegistrationIndex];
+        
+        return (deviceId, registeredDevices[deviceId]);
+    }
+    
+    function isStringEmpty(string stringToCheck) private returns (bool) {
+        return bytes(stringToCheck).length == 0; 
     }
 }
